d047b70fefe532b633f4f7b75ae80e37fc7e70ca
David Robinson <davecrob2@gmail.com>
2017-10-18 21:14:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route('/home')
 def hello_world():
-  return(render_template('form.html')
+  return render_template('form.html')
   
 if __name__ == '__main__':
   app.run()
